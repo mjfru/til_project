696e77ad1e2c20ed62fcb28ddc8738370d09010e
Matt Frutchey <mjfru201@gmail.com>
2024-05-24 08:38:41 -0400

    finish fact list component, create fact, and filter buttons

---
+++ til_project/til_project_react/src/Fact.jsx
@@ -0,0 +1,34 @@
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
+function Fact({ fact }) {
+
+
+
+  console.log(fact);
+  return (
+        <li className="fact">
+            <p>{fact.text}
+              <a className="source" target="_blank" href={fact.source}>Source</a>
+            </p>
+            <span 
+              className="tag" 
+              style={{backgroundColor: CATEGORIES.find((cat) => cat.name === fact.category)?.color}}>{fact.category}</span>
+            <div className="vote-buttons">
+              <button>👍 {fact.votesInteresting}</button>
+              <button>🤯 {fact.votesMindblowing}</button>
+              <button>⛔ {fact.votesFalse}</button>
+            </div>
+          </li>
+  )
+}
+
+export default Fact;
--- til_project/til_project_react/src/FactList.jsx
+++ til_project/til_project_react/src/FactList.jsx
@@ -1,16 +1,7 @@
+import Fact from "./Fact";
+
 function List() {
-
-  const CATEGORIES = [
-    { name: "technology", color: "#3b82f6" },
-    { name: "science", color: "#16a34a" },
-    { name: "finance", color: "#ef4444" },
-    { name: "society", color: "#eab308" },
-    { name: "entertainment", color: "#db2777" },
-    { name: "health", color: "#14b8a6" },
-    { name: "history", color: "#f97316" },
-    { name: "news", color: "#8b5cf6" },
-  ];
-
+  
   const initialFacts = [
     {
       id: 1,
@@ -40,21 +31,10 @@ function List() {
     <section>
       <ul className="facts-list">
         {facts.map((fact) => (
-          <li className="fact" key={fact.id}>
-            <p>{fact.text}
-              <a className="source" target="_blank" href={fact.source}>Source</a>
-            </p>
-            <span 
-              className="tag" 
-              style={{backgroundColor: CATEGORIES.find((cat) => cat.name === fact.category)?.color}}>{fact.category}</span>
-            <div className="vote-buttons">
-              <button>👍 {fact.votesInteresting}</button>
-              <button>🤯 {fact.votesMindblowing}</button>
-              <button>⛔ {fact.votesFalse}</button>
-            </div>
-          </li>
+          <Fact key={fact.id} fact={fact}/>
         ))}
       </ul>
+      <p>There are {initialFacts.length} facts in the database.</p>
     </section>
   )
 }
