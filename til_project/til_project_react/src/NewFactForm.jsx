a3e3c312ea5074b759f7835052efcad13688442b
Matt Frutchey <mjfru201@gmail.com>
2024-05-26 12:56:15 -0400

    finish category list, add additional interactivity with react

--- til_project/til_project_react/src/NewFactForm.jsx
+++ til_project/til_project_react/src/NewFactForm.jsx
@@ -1,6 +1,46 @@
+import { useState } from "react";
+import CATEGORIES from "../utilities/categories";
+
 function NewFactForm() {
+  const [text, setText] = useState("");
+  const [source, setSource] = useState("");
+  const [category, setCategory] = useState("");
+
+  // text.length is referring to the state of text
+  const textLength = text.length;
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(text, source, category);
+  }
+
   return (
-    <form action="" className="fact-form">Fact Form</form>
+    <form className="fact-form" onSubmit={handleSubmit}>
+      <input 
+        type="text"
+        placeholder="Share a fact with the world..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}/>
+
+      <span>{200 - textLength}</span>
+      <input 
+        type="text"
+        placeholder="Trustworthy source..."
+        value={source}
+        onChange={(e) => setSource(e.target.value)}/>
+      
+      <select 
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}>
+          {/* Below has the value of "" so it will match state and display by default */}
+        <option value="">Choose category:</option>
+        {CATEGORIES.map((category) => 
+          <option key={category.name} value={category.name}>
+            {category.name.toUpperCase()}
+          </option>)}
+      </select>
+      <button className="btn btn-large">Post</button>
+    </form>
   )
 }
 
